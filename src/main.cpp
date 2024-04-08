de665286d4a4f24a08a0c30289ce6eab6a5b827e
SaphirDeFeu <saphirdefeu@proton.me>
2024-04-08 21:56:49 +0200

    Rudimentary CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,73 @@
 #include<iostream>
+#include<vector>
+#include<string>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+
+using std::vector;
+using std::string;
+
+const string VERSION = "1.0.0";
+
+template <typename T>
+bool vectorHasElement(vector<T>* vec, const T& element) {
+  int len = vec->size();
+  for(int i = 0; i < len; i++) {
+    if(vec->at(i) == element) {
+      return true;
+    }
+  }
+  return false;
 }
+
+vector<string> processCLIArguments(const int& argc, char** argv) {
+  vector<string> result = vector<string>();
+
+  if(argc == 1) {
+    result.push_back("-h");
+  }
+
+  for(int i = 0; i < argc; i++) {
+    string arg = string(argv[i]);
+
+    if(arg == "-h" || arg == "--help") {
+      result.push_back("-h");
+      continue;
+    }
+
+    if(arg == "-V" || arg == "--version") {
+      result.push_back("-V");
+      continue;
+    }
+
+  }
+
+  return result;
+};
+
+void showHelp() {
+  std::cout << "CCreate - C/C++ Package manager and build system - by TSMStudios\n" << std::endl;
+  std::cout << "Usage: ccreate [options] [command]\n" << std::endl;
+  std::cout << "Options:" << std::endl;
+  std::cout << "  -h, --help      Display this help message" << std::endl;
+  std::cout << "  -V, --version   Display CCreate's current version" << std::endl;
+  std::cout << "\nCommands:" << std::endl;
+  std::cout << "  new             Create a new CCreate project" << std::endl;
+  return;
+}
+
+
+int main(int argc, char** argv) {
+  vector<string> processedArgs = processCLIArguments(argc, argv);
+
+  if(vectorHasElement<string>(&processedArgs, string("-h"))) {
+    showHelp();
+    return 0;
+  }
+
+  if(vectorHasElement<string>(&processedArgs, string("-V"))) {
+    std::cout << "ccreate " << VERSION << std::endl;
+    return 0;
+  }
+
+  return 0;
+};
