153fc0117d93c79f618e1173870d6de135b94a65
SaphirDeFeu <saphirdefeu@proton.me>
2024-04-12 14:21:59 +0200

    Added toml11 library

--- include/build/mod.h
+++ include/build/mod.h
@@ -3,6 +3,8 @@
 
 #include<filesystem>
 
+void delete_carriage_returns(std::string* text);
+
 int build_project(const std::filesystem::path& cwd);
 
 #endif
--- src/build/mod.cpp
+++ src/build/mod.cpp
@@ -1,31 +1,36 @@
 #include<iostream>
 #include<filesystem>
 #include<string>
+#include<vector>
 #include<fstream>
 
 #include"build/mod.h"
-#include"build/toml.h"
+#include"toml.hpp"
 
 using std::string;
+using std::vector;
 using std::ifstream;
+
+void delete_carriage_returns(string* text) {
+  string toReplace("\r");
+
+  size_t pos = text->find(toReplace);
+  text->replace(pos, toReplace.length(), "");
+} 
 
 int build_project(const std::filesystem::path& cwd) {
   string line;
-  string text = "";
+  vector<string> lines;
 
-  ifstream config_file((cwd / "CCreate.toml").c_str());
+  ifstream config_file(cwd / "CCreate.toml");
 
   if(!config_file.is_open()) {
     std::cerr << "Could not build project because CCreate.toml cannot be found." << std::endl;
     return 1;
   }
 
-  while(std::getline(config_file, line)) {
-    text = text + line + "\n";
-  }
-
-  CCreateConfig config = parse_ccreate_config(text);
-
+  const toml::value data = toml::parse(config_file, cwd / "CCreate.toml");
   config_file.close();
+  
   return 0;
 }
