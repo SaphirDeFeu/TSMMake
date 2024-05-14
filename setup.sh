e6a7ae59bc64d3ae373b1402629587c15a87a9b5
SaphirDeFeu <saphirdefeu@proton.me>
2024-05-14 20:52:24 +0200

    update c/cpp ci

---
+++ setup.sh
@@ -0,0 +1,10 @@
+echo "Set up build files"
+
+if [ -d "./target" ]; then
+  echo "Build folder already up"
+else
+  mkdir target
+  mkdir target/obj
+  mkdir target/obj/build
+  mkdir target/obj/parser
+fi
