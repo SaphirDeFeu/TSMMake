44e17336f074850418931d9d50f82a1e3f7db670
SaphirDeFeu <saphirdefeu@proton.me>
2024-05-20 10:47:19 +0200

    removed repetitive code

--- include/platform.hpp
+++ include/platform.hpp
@@ -2,20 +2,8 @@
 #ifndef PLATFORM_DEPENDENT_CODE
 #define PLATFORM_DEPENDENT_CODE
 
-#ifdef _WIN32
-
 const std::string PATH_SEPARATOR = "\\";
 
 std::string get_exec_path();
-
-#endif
-
-#ifdef __linux__
-
-const std::string PATH_SEPARATOR = "/";
-
-std::string get_exec_path();
-
-#endif
 
 #endif
